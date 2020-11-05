6046d346b9d4642e981a881419d18ff773fb91ff
orama254 <orama209@gmail.com>
2020-11-05 10:30:24 +0300

    add countdown functionality in JS

--- script.js
+++ script.js
@@ -9,5 +9,14 @@ const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
+
+const d = Math.floor(gap / (day));
+const h = Math.floor((gap % (day)) / (hour));
+const m = Math.floor((gap % (hour)) / (minute));
+const s = Math.floor((gap %  (minute)) / (second));
+document .getElementById('day').innerText = d;
+document.getElementById('hour').innerText = h;
+document .getElementById('minute').innerText = m;
+document.getElementById('second').innerText = s;
 }
 
